40abfb39065ced38ddc4c83ff170d9de48d9f2b9
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-15 12:01:07 -0700

    Task 2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
 char ch;
 
 for(ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 
-for (ch = 'A'; ch <= 'Z'; ch++)
+for(ch = 'A'; ch <= 'Z'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
