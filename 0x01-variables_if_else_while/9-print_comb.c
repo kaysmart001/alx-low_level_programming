753ac3ae3fbd4ff23dda5132e694cce976b8d930
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-16 13:38:01 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,14 @@ int main(void)
 {
 int x;
 for (x = 0; x < 10; x++)
-   {			            putchar(x + '0');
-	   if (x < 9)
-	   {
+{
+	putchar(x + '0');
+	if (x < 9)
+	{
 putchar(',');
 putchar(' ');
-	   }
-   }
+	}
+}
 putchar('\n');
 return (0);
 }
