fe96ff4c1890528ceede6e2970dd10f2c2010adf
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-16 10:32:47 -0700

    Task 2

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
 int num;
 
-for (num = 0; num < 10; num++)
-	printf ("%d", num);
-	printf("\n");
+for(num = 0; num < 10; num++)
+	printf("%d\n", num);
+
 return (0);
 }
