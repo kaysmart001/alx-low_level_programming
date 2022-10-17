3acc5ed57b7d277b1dc952e261e1db31a15456ea
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-17 02:59:28 -0700

    Task 9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+  *main - print numbers  hexadecimal numbers
+  *Return: Return 0
+  */
+
 int main(void)
 {
 int a;
-for(a = 0; a <= 15; a++)
+for (a = 0; a <= 15; a++)
 putchar(0xa);
 putchar('\n');
 
