1ec94821ec4a48d90ed6f63a8b9093d7c3631287
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-18 14:54:16 -0700

    Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - print putchar
+ * Return:0
+*/
+
+int main(void)
+
+{
+int put = 0;
+
+
+	if (put < 1)
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is my readme for my 0x02-functions_nested_loops project at ALX 
