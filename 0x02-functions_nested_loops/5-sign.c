4d1c830aa47e03a2d704bc5ff099d2f98e6d1c37
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-19 08:18:28 -0700

    Task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
-
-		_putchar(+);
+{
+	_putchar(+);
 	return (1);
+}
+	else
 
-	else if (n < 0)
+{
+	if (n < 0)
 		_putchar(-);
 	return (1);
 
 	else 
 		_putchar(0);
 	return (0);
+}
 }
