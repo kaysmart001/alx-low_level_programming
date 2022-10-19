9db381318881efc0cfee2bdf52f69eb92c0c0477
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-19 08:13:42 -0700

    Task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_sign(int n) - show and print If a number is positive, negative or zero
+  * Return: 1, 0
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+
+		_putchar(+);
+	return (1);
+
+	else if (n < 0)
+		_putchar(-);
+	return (1);
+
+	else 
+		_putchar(0);
+	return (0);
+}
