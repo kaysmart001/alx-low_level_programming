f5df1e40cc220ef7c1e70065217c22047d4482c7
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-19 21:47:30 -0700

    Task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,48 +2,29 @@
 
 
 /**
-
-   * jack_bauer - prints every minute of the day
-
-    * Description: prints every minute of the day
-
-     * Return: void
-
-      */
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
+ */
 
 void jack_bauer(void)
-
 {
-
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
-
 	hours_max = 58;
-
 	hours_tens = '0';
-
 	while (hours_tens < '3')
-
-	{
-
+{
 	if (hours_tens == '2')
 {
-
 	hours_max = '4';
-
 }
-
 hours_ones = '0';
-
 while (hours_ones < hours_max)
-
-{
+}
 	minutes_tens = '0';
-
 while (minutes_tens < '6')
-
 {
 	minutes_ones = '0';
-
 while (minutes_ones < 58)
 {
 	_putchar(hours_tens);
@@ -61,7 +42,6 @@ minutes_tens++;
 minutes_tens = '0';
 hours_ones++;
 }
-
 hours_ones = '0';
 hours_tens++;
 }
