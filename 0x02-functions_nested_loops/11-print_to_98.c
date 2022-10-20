50dff414c5a7b1667ab48258bb6a10783165439c
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-19 21:54:12 -0700

    Task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,8 +8,6 @@
 int main(void)
 
 {
-	void print_to_98(int n)
-{
 	print_times_table(3);
 	_putchar('\n');
 	print_times_table(5);
@@ -17,6 +15,6 @@ int main(void)
 	print_times_table(98);
 	_putchar('\n');
 	print_times_table(12);
-}
+
 return (0);
 }
