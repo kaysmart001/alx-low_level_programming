6a73e32ae01bc2a38584ef872a7d89405d3f7763
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-10-21 11:28:27 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most numbers
+ @i
+ */
+
+void print_most_numbers(void)
+{
+
+	int i;
+
+For (i = 0, i <= 10, I++)
+{
+	if (i != 2 && i != 4)
+
+_putchar('i');
+_putchar('\n')
+}
+
+}
