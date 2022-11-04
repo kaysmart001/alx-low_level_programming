ab1c0935a4ee616a200bcd633c8e396def6655b9
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-11-04 15:24:58 -0700

    task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+* _islower - to print a to z in lower case
+* @c - the letters 
+* Return: always 0
+*/
+
+int _isupper(int c)
+{
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
+
+	return (0);
+}
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o *.o
 ar rc liball.a *.o
