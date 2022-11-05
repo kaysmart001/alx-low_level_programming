bed156a6bcd3363e9067e0c8ff7f372f1805f982
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-11-05 00:01:23 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
   * main - to Write a program that prints its name
-  * @argc - the number of arguement
-* @argv[] - the array of size of argc
+  * @argc:  the number of arguement
+* @argv[]:  the array of size of argc
 * Return: always 0
   */
 
 int main(int argc, char *argv[])
 {
-	_putchar(argv[0]);
+	printf("%s\n",argv[0]);
 
 	return (0);
 }
