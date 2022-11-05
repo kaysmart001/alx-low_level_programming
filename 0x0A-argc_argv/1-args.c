1bbad03cda6ec07df084cceb38da2ef426f7e1f1
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-11-05 00:31:37 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-  * main - to Write a program that prints the number of arguments passed into it.
+ * main - to Write a program that prints the number of arguments passed into it
   * @argc: the number of arguemrnts
   * @argv: the array of the size of argc
   * Return: always 0
