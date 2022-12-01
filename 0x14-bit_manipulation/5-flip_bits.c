b42fc17c554e009637332aac74ead98a45aa229d
Luwoye Kayode <luwoyekayode99@gmail.com>
2022-12-01 11:20:02 -0800

    last task

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * clear_bit - Sets the value of a bit at a given index to 0.
- * @n: A pointer to the bit.
- * @index: The index to set the value at - indices start at 0.
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
  *
- * Return: If an error occurs - -1.
- *         Otherwise - 1.
+ * Return: The necessary number of bits to flip to get from n to m.
  */
 
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
+	unsigned long int xor = n ^ m, bits = 0;
 
-	*n &= (1 << index);
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
 
-	return (1);
+	return (bits);
 }
